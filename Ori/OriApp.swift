4221367a023e77bf4c071eb2335818e8a35558e3
dkon <dkon0722@gmail.com>
2026-09-13 09:19:25 +0900

    Initial Commit

---
+++ Ori/OriApp.swift
@@ -0,0 +1,17 @@
+//
+//  OriApp.swift
+//  Ori
+//
+//  Created by 今大輔 on 2026/09/13.
+//
+
+import SwiftUI
+
+@main
+struct OriApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
